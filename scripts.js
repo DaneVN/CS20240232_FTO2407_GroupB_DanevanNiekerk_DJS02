5528ad2215fcf2e718aa2544108b7ba373744819
DaneVN <Dane.vN23@gmail.com>
2024-10-25 16:35:12 +0200

    User Stories|Error Handling DONE

--- scripts.js
+++ scripts.js
@@ -5,5 +5,38 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const calcResult = Math.floor(dividend / divider);
+  //still need to customise when the results are displayed and when they
+  //don't happen
+
+  try {
+    if (!dividend || !divider) {
+      throw `Division not performed. Both values are required in inputs. 
+      Try again`;
+    } else {
+      result.innerText = calcResult;
+    }
+  } catch (err) {
+    result.innerText = err;
+  }
+
+  try {
+    if (divider == 0) {
+      throw new Error(
+        "Division not performed. Invalid number provided. Try again"
+      );
+    } else {
+      result.innerText = calcResult;
+    }
+  } catch (err) {
+    console.error(err.stack);
+    result.innerText = err.message;
+  }
+  try {
+    if (isNaN(dividend) || isNaN(divider)) {
+      throw new Error("Something critical went wrong. Please reload the page");
+    }
+  } catch (err) {
+    document.body.innerHTML = `<h2>${err.message}</h2>`;
+  }
 });
